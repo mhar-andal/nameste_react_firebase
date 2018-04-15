702a63a48e896936d046d768b6548d630bd8ccc1
Mhar Andal <mharandal96@gmail.com>
2018-04-15 15:15:38 -0500

    :sparkles:Adds material UI provider:sparkles:

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { render } from 'react-dom';
 
 import 'styles/styles.scss';
 
 import Homepage from './components/Homepage';
 import Header from './components/Header';
-import Signup from './components/Signup';
+import Signup from './containers/Signup';
 import NotFound from './components/NotFound';
-
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      authUser: null,
+      authUser: null
     };
   }
 
   render() {
     return (
-      <Router>
-        <div>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/signup" component={Signup} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </Router>
+      <MuiThemeProvider>
+        <Router>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/signup" component={Signup} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
